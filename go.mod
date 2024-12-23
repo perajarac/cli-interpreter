581b1df232c62ee30b4006ab7d059703462b4fe1
perajarac <petarmijatovic99@gmail.com>
2024-12-23 21:20:00 +0100

    Memory update curently cant be added, because lack of knowledge with Console

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module cli_interpreter
 
 go 1.23.3
-
-require (
-	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-)
--- main.go
+++ main.go
@@ -5,10 +5,7 @@ import (
 	"cli_interpreter/memory"
 	read "cli_interpreter/reader"
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/eiannone/keyboard"
 )
 
 var mem *memory.Memory = memory.New()
@@ -18,48 +15,6 @@ var reader *read.Reader = &read.Reader{
 	Scanner: bufio.NewReader(os.Stdin),
 }
 
-func listen_for_arrow_keys(channel chan string) {
-	// Initialize keyboard listener
-	err := keyboard.Open()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer keyboard.Close()
-
-	// Listen for arrow key presses
-	for {
-		_, key, err := keyboard.GetKey()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if key == keyboard.KeyEsc {
-			break
-		}
-
-		switch key {
-		case keyboard.KeyArrowUp:
-			fmt.Println("Up")
-			channel <- mem.Up()
-		case keyboard.KeyArrowDown:
-			fmt.Println("Down")
-			channel <- mem.Down()
-		default:
-			fmt.Println("ovde sam")
-		}
-
-	}
-}
-
-func read_coomands(channel chan string) {
-	var cmd string
-	for {
-		fmt.Print(reader.Sign)
-		cmd = reader.Read_command()
-		channel <- cmd
-	}
-}
-
 func main() {
 
 	clear := func() {
@@ -67,18 +22,15 @@ func main() {
 		reader.Clear()
 	}
 
-	command := make(chan string)
-
-	go read_coomands(command)
-	go listen_for_arrow_keys(command)
-
-	for cmd := range command {
-		err := reader.Execute(cmd)
+	for {
+		fmt.Print(reader.Sign)
+		command := reader.Read_command()
+		err := reader.Execute(command)
 		if err != nil {
-			fmt.Println("Error occurred:", err)
+			fmt.Println("Error occured: ", err)
 		}
-		mem.Push(cmd)
 		clear()
+		mem.Push(command)
 	}
 
 }
